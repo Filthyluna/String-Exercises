e5b1c6ecd68ad5b7d4100cfbc69a300a1b66ade8
Mel <56780129+Filthyluna@users.noreply.github.com>
2021-09-20 22:06:31 -0400

    Updating string.js

--- string.js
+++ string.js
@@ -1,3 +1,5 @@
+//GROUP MEMBERS: Khazine, George, Melanie
+
 //DrEvil
 //create a function called DrEvil. It should take a single argument, an amount
 //and return '<amount> dollars', except it will add '(pinky)' at the end if 
@@ -6,8 +8,20 @@
 // DrEvil(1000000): 1000000 dollars (pinky)
 // answer below:
 
+function DrEvil(number) {
+  //checks if number is 1 million dollars first
+  if (number == 1000000) {
+    //if true it returns the string with pinky at the end
+    return console.log(number + " dollars (pinky)");
+  }
+  else {
+    //if not true it returns the string without pinky at the end
+    return console.log(number + " dollars");
+  }
+}
 
-
+DrEvil(10); //reg test
+DrEvil(1000000);//pinky test
 
 
 
@@ -20,7 +34,24 @@
 //mixUp('dog', 'dinner'): 'dig donner'
 //write answer below 
 
+function mixUp(str1, str2) {
+  //Slice out first two characters - set them to their own variables
+  let newstr1 = str1.slice(0, 2);
+  let newstr2 = str2.slice(0, 2);
 
+  //Replace these characters with ones from the other string - set them to their new strings
+  let updatedstr1 = str1.replace(newstr1, newstr2);
+  let updatedstr2 = str2.replace(newstr2, newstr1);
+
+  //Create variable that holds the concatenation 
+  const conc = updatedstr1 + " " + updatedstr2;
+
+  //return it to print
+  return console.log(conc);
+}
+
+mixUp("mix", "pod"); //test 1
+mixUp("dog", "dinner");//test 2
 
 
 
@@ -30,6 +61,14 @@
 //character have been replaced with '*', except for the character itself. 
 //fixstart('babble'): 'ba**le'
 //write answer below
+function fixStart(string) {
+  var firstChar = string.charAt(0);
+  let newString = firstChar + string.slice(1).replace(new RegExp(firstChar, "g"), '*');
+  
+  return newString;
+}
+
+console.log(fixStart("babble"));
 
 
 
@@ -45,7 +84,24 @@
 //verbing('go'): 'go'
 //write answer below
 
+function verbing(string) {
+  if (string.length < 3) {
+    return string;
+  }
+  else if (string.length >= 3 && string.slice(-3) != "ing") {
+    if (string.endsWith("m")) {
+      return string + "ming";
+    }
+    else {
+      return string + "ing";
+    }
+  }
+  else if (string.length >= 3 && string.slice(-3) == "ing") {
+    return string + "ly";
+  }
+}
 
+console.log(verbing("swim"));
 
 
 
@@ -63,3 +119,16 @@
 //notBad('This movie is not so bad!'): 'This movie is good!'
 //notBad('This dinner is bad!'): 'This dinner is bad!'
 //write answer below 
+function notBad(string) {
+  var notPlace = string.indexOf("not");
+  var badPlace = string.indexOf("bad");
+  if (notPlace == -1 || badPlace == -1 || badPlace < notPlace) {
+    return string;
+  }
+  else {
+    let newString = string.slice(0, notPlace) + "good" + string.slice(badPlace + 3);
+    return newString;
+  }
+}
+
+console.log(notBad("This dinner is not that bad!"));
